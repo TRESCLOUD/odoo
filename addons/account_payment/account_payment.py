7ab30ccc4e11161485fa520fe8082a1edbb80462
patricio <patricio@debiansamsung>
2015-11-28 17:17:26 -0500

    Corregido reescritura de la funcion fields_get
    https://trescloud.atlassian.net/browse/FDU-1145

--- addons/account_payment/account_payment.py
+++ addons/account_payment/account_payment.py
@@ -413,8 +413,11 @@ class payment_line(osv.osv):
                         break
         return {'value': data}
 
-    def fields_get(self, cr, uid, fields=None, context=None):
-        res = super(payment_line, self).fields_get(cr, uid, fields, context)
+    def fields_get(self, cr, uid, fields=None, context=None, allfields=None):
+        # name of argument fields incorrect
+        if allfields:
+            fields=allfields
+        res = super(payment_line, self).fields_get(cr, uid, allfields=fields, context=context)
         if 'communication2' in res:
             res['communication2'].setdefault('states', {})
             res['communication2']['states']['structured'] = [('readonly', True)]
