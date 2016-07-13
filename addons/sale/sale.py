5d6678c1f853dbdf086823efcf215ba1f86bc8a3
José Miguel <jose.rivero@trescloud.com>
2016-07-13 14:13:41 -0500

    [FIX][account, sale]: Error en el calculo de impuestos

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -103,7 +103,7 @@ class sale_order(osv.osv):
             cur = order.pricelist_id.currency_id
             for line in order.order_line:
                 val1 += line.price_subtotal
-                val += self._amount_line_tax(cr, uid, line, context=context)
+                val += cur_obj.round(cr, uid, cur, self._amount_line_tax(cr, uid, line, context=context))
             res[order.id]['amount_tax'] = cur_obj.round(cr, uid, cur, val)
             res[order.id]['amount_untaxed'] = cur_obj.round(cr, uid, cur, val1)
             res[order.id]['amount_total'] = res[order.id]['amount_untaxed'] + res[order.id]['amount_tax']
