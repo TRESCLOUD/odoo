ded97a85b6d7c8220e3b62dfadb8be7cf33cfb4d
Andres Calle <andres.calle@trescloud.com>
2022-05-31 01:23:42 -0500

    [IMP] l10n_ec: Upgrade for missing data, validations, etc
    Data improvements:
    - Added missing tax groups
    - Added VAT 8%, used for tourism operations on special hollidays
    - Fixes base percentage on Withhold VAT taxes
    - Corrected tax name from Iva to IVA
    - Improves SRI paymment options, with sequence widget and inactavtes
    deprecated payment options.
    - Fixes short code of document types
    - Corrects document type name for insurance
    - Adds withhold document type, to be implemented in separated module
    
    
    Other improvements:
    - Automatic setup of several journals when creating new companies
    - Adds restrictions to ensure proper setup of ecuadorian journals
    - Improves ecuadorian journal form

--- addons/l10n_ec/models/l10n_latam_document_type.py
+++ addons/l10n_ec/models/l10n_latam_document_type.py
@@ -7,10 +7,11 @@ import re
 
 class L10nLatamDocumentType(models.Model):
     _inherit = "l10n_latam.document.type"
-
+    
     internal_type = fields.Selection(
         selection_add=[
             ("purchase_liquidation", "Purchase Liquidation"),
+            ("withhold", "Withhold"),
         ]
     )
 
